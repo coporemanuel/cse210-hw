2aaac7b122c98cea28578d83427b0408a0fd5e9a
coporemanuel <jcoporecutipa@byupathway.edu>
2025-05-30 21:50:04 -0400

    youtubevideos and screenshot ready

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        Video video1 = new Video("C# for Beginners", "TechAcademy", 1200);
+        video1.AddComment(new Comment("Alison", "Great tutorial, very clear!"));
+        video1.AddComment(new Comment("Neil", "I finally understand classes, thanks!"));
+        video1.AddComment(new Comment("Manuel", "More examples would be great!"));
+        videos.Add(video1);
+
+        Video video2 = new Video("Advanced OOP Concepts", "CodeMaster", 1800);
+        video2.AddComment(new Comment("David", "Fantastic explanation of polymorphism!"));
+        video2.AddComment(new Comment("Eva", "Encapsulation is now my favorite thing."));
+        video2.AddComment(new Comment("Franco", "Could you cover abstract classes next?"));
+        videos.Add(video2);
+
+        Video video3 = new Video("C# Design Patterns", "DevInsights", 2400);
+        video3.AddComment(new Comment("Graciela", "Singleton pattern always confuses me."));
+        video3.AddComment(new Comment("Francisco", "Great breakdown of factory patterns!"));
+        video3.AddComment(new Comment("Ines", "Very well-structured, thanks!"));
+        videos.Add(video3);
+
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+        }
     }
 }
